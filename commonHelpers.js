e8b18fe31e5e2fcf1eb9a4b579bcdc62c8c29033
kiianskii <151513871+kiianskii@users.noreply.github.com>
2024-03-07 18:13:44 +0000

    Deploying to gh-pages from @ kiianskii/goit-js-hw-11@e16d78229497dac7bd6ff481006113080dcbdc1f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,31 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+import{S as c,i as u}from"./assets/vendor-7659544d.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))r(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const i of s.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function a(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function r(t){if(t.ep)return;t.ep=!0;const s=a(t);fetch(t.href,s)}})();function d(e){const o="https://pixabay.com/",a="api/",r=new URLSearchParams({key:"42734676-8c749a784e7b90411d6581e4f",q:e,image_type:"photo",orientation:"horizontal",safesearch:!0}),t=`${o}${a}?${r}`;return fetch(t).then(s=>{if(!s.ok)throw new Error(s.status);return s.json()})}const h=new c(".gallery a",{captionsData:"alt",captionDelay:250}),f=document.querySelector(".gallery");function m(e){const o=p(e);f.innerHTML=o,h.refresh()}function p(e){return e.map(g).join(`
+`)}function g(e){return`<li class="gallery-item">
+                <a class="gallery-link" href="${e.largeImageURL}">
+                    <img
+                        class="gallery-image"
+                        src="${e.webformatURL}"
+                        alt="${e.tags}"
+                        width="${e.previewWidth}"
+                        height="${e.previewHeight}"
+                    />
+                </a>
+                <ul class="description">
+                        <li class="desc-item">
+                            <h3 class="des-head">Likes</h3>
+                            <p>${e.likes}</p>
+                        </li>
+                        <li class="desc-item">
+                            <h3 class="des-head">Views</h3>
+                            <p>${e.views}</p>
+                        </li>
+                        <li class="desc-item">
+                            <h3 class="des-head">Comments</h3>
+                            <p>${e.comments}</p>
+                        </li>
+                        <li class="desc-item">
+                            <h3 class="des-head">Downloads</h3>
+                            <p>${e.downloads}</p>
+                        </li>
+                    </ul>
+            </li>`}const l=document.querySelector(".loader"),y={message:"Sorry, there are no images matching your search query. Please try again!",messageSize:"16px",messageLineHeight:"24px",messageColor:"#fff",backgroundColor:"#EF4040",progressBarColor:"#B51B1B",theme:"dark",position:"topRight",class:"message"};function L(e){l.classList.remove("is-hide")}function n(e){l.classList.add("is-hide")}function w(){u.show(y)}const $=document.querySelector(".search-form");$.addEventListener("submit",b);function b(e){e.preventDefault(),L();const a=e.target.elements.data.value.trim().split(" ").filter(r=>r).join("+");d(a).then(r=>{[...r.hits].length===0?(n(),w()):m(r.hits)}).catch(console.log).finally(()=>{n()}),e.target.reset()}
 //# sourceMappingURL=commonHelpers.js.map
